34ae0ecc741a1296c2fc3cf142b7416ab9b6c112
Sego11 <sammybaffoe123@gmail.com>
2024-07-30 16:41:51 +0000

    initial commit

---
+++ src/test/java/com/Shoply_Backend/ShoplyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shoply_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
